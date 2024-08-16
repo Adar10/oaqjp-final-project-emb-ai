8dcd262905b5de04591a759fc5c3b436ba9a24e2
Adar10 <adardonmezz@gmail.com>
2024-08-16 13:05:00 -0400

    project done

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+"""
+Server code
+
+"""
+
 from flask import Flask, request, jsonify
 from EmotionDetection import emotion_detector
 
@@ -5,16 +10,19 @@ app = Flask(__name__)
 
 @app.route('/emotionDetector', methods=['POST'])
 def detect_emotion():
-    print("Request received")  # Debugging line
+
+    """
+    Handles POST requests to the /emotionDetector endpoint.
+    
+    """
+
     data = request.get_json()
 
     text_to_analyze = data["text"]
     if not text_to_analyze:
-        return jsonify({"response": "No text provided"}), 400
+        return jsonify({"response": "Invalid text! Please try again!"}), 400
 
     result = emotion_detector(text_to_analyze)
-    print("Emotion detection result:", result)  # Debugging line
-    
     if result['dominant_emotion'] is None:
         return jsonify({"response": "Invalid text! Please try again!"}), 400
 
